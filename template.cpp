bc509e921fad86b494424b49f3586ffc13da78f8
vitorguidi <johndoe@example.com>
2018-05-13 15:23:05 -0300

    mend

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+#define pq priority_queue	
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define pq priority_queue
+#define fr(i,n)	for(int i=0;i<n;i++)
+#define frr(i,n) for(int i=1;i<=n;i++)
+
+#define ms(x, v) memset(x, v, sizeof x)
+#define all(x)	x.begin(),x.end()
+
+#define dbg(x) cout << #x << " = " << x << endl
+#define olar() cout << "olaarr" << endl
+#define gnl cout << endl
+
+const int INF = 0x3f3f3f3f;
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+int main(){
+
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+}
+
+
