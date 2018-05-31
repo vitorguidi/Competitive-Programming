76151cb9954121fe68d389067b1545219761e17d
vitorguidi <johndoe@example.com>
2018-05-31 14:19:53 -0300

    atualizando template

--- template.cpp
+++ template.cpp
@@ -1,35 +1,35 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-#define fst first
-#define snd second
 #define pb push_back
 #define mp make_pair
-#define pq priority_queue	
-#define vi vector<int>
-#define vii vector<pair<int,int>>
+#define fst first
+#define snd second
+
+#define fr(i,n) for(int i=0;i<n;i++)
+#define frr(i,n)	for(int i=1;i<=n;i++)
+
+#define ms(x,i)	memset(x,i,sizeof(x))
+#define dbg(x)	cout << #x << " = " << x << endl
 #define pq priority_queue
-#define fr(i,n)	for(int i=0;i<n;i++)
-#define frr(i,n) for(int i=1;i<=n;i++)
+#define grtp greater< pair<int,int> >
 
-#define ms(x, v) memset(x, v, sizeof x)
-#define all(x)	x.begin(),x.end()
-
-#define dbg(x) cout << #x << " = " << x << endl
-#define olar() cout << "olaarr" << endl
 #define gnl cout << endl
+#define olar cout << "olar" << endl
+
+typedef pair<int,int> pii;
+typedef pair<long long int,long long int> pll;
+typedef vector<int> vi;
+typedef vector< pair<int,int> > vii;
+typedef long long int ll;
 
 const int INF = 0x3f3f3f3f;
-
-typedef long long int ll;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
+const ll llINF = 0x3f3f3f3f3f3f3f;
 
 int main(){
-
-    std::ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
 }
-
 
