e7c6db20174c671225e7dcfb2dc5fc4ab9c0df97
vitorguidi <johndoe@example.com>
2018-06-04 00:08:52 -0300

    template atualizado

--- template.cpp
+++ template.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #define fst first
 #define snd second
 
-#define fr(i,n) for(int i=0;i<n;i++)
+#define fr(i,n) 	for(int i=0;i<n;i++)
 #define frr(i,n)	for(int i=1;i<=n;i++)
 
 #define ms(x,i)	memset(x,i,sizeof(x))
@@ -16,6 +16,7 @@ using namespace std;
 
 #define gnl cout << endl
 #define olar cout << "olar" << endl
+#define gottagofast ios_base::sync_with_stdio(false); cin.tie(NULL)
 
 typedef pair<int,int> pii;
 typedef pair<long long int,long long int> pll;
@@ -28,8 +29,7 @@ const ll llINF = 0x3f3f3f3f3f3f3f;
 
 int main(){
     
-    ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    
 
 }
 
