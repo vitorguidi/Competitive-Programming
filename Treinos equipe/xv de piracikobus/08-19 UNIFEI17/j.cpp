b88efd710f02fa765911db7e94277d40ab82991c
guidi <vitorguidi@gmail.com>
2018-08-20 14:28:24 -0300

    adicionado upsolving do J da UNIFEI17 seletiva

--- Treinos equipe/xv de piracikobus/08-19 UNIFEI17/j.cpp
+++ Treinos equipe/xv de piracikobus/08-19 UNIFEI17/j.cpp
@@ -14,6 +14,7 @@ using namespace std;
 #define all(x)	x.begin(),x.end()
 
 #define gnl cout << endl
+#define otp(x) cout << x << endl
 #define olar cout << "olar" << endl
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
 
@@ -25,28 +26,31 @@ typedef pair<ll,ll> pll;
 
 const int INF = 0x3f3f3f3f;
 const ll llINF = 0x3f3f3f3f3f3f3f;
+const int maxn = 1e7+9;
 
-int v[100100];
-vi seg[400400];
+int v[100200];
+vi seg[400800];
+double memo[82][82][82][163];
 
-vi merge(vi& a, vi& b){
+vi merge(vi &a, vi &b){
 
 	vi c;
 
-	int inia=0, inib=0;
-	int la=a.size(),lb=b.size();
+	int inia=0,inib=0;
+	int la = a.size(), lb = b.size();
 
-	while(inia<la && inib<lb){
+	while(inia<a.size() && inib<b.size()){
 	
 		if(a[inia]<=b[inib]){
 			c.pb(a[inia]);
 			inia++;
 		}
-
+	
 		else{
 			c.pb(b[inib]);
 			inib++;
 		}
+
 	}
 
 	for(int i=inia;i<la;i++)
@@ -57,7 +61,7 @@ vi merge(vi& a, vi& b){
 
 	return c;
 
-}
+} 
 
 void build(int node, int l, int r){
 
@@ -73,13 +77,13 @@ void build(int node, int l, int r){
 	build(2*node,l,mid);
 	build(2*node+1,mid+1,r);
 
-	seg[node] = merge( seg[2*node] , seg[2*node+1] );
+	seg[node]=merge(seg[2*node],seg[2*node+1]);
 
 }
 
 vi query(int node, int l, int r, int a, int b){
 
-	if(r<a || l>b || l>r){
+	if(l>r || r<a || l>b){
 		vi aux;
 		return aux;
 	}
@@ -96,35 +100,69 @@ vi query(int node, int l, int r, int a, int b){
 
 }
 
-ll solve(int len, int k){
+double solve(int b, int r, int z, int check ){
+
+	if(z==0){
+		
+		if(check>80)
+			return 1.0;
+		
+		return 0.0;
+	}
+
+	double &pdm = memo[b][r][z][check];
+	
+	if(pdm!=-5.0)
+		return pdm;
+
+	pdm = 0.0;
+
+	if(b)
+		pdm+=solve(b-1,r,z-1,check+1)*( double(b)/double(b+r) );
+	if(r)
+		pdm+=solve(b,r-1,z-1,check-1)*( double(r)/double(b+r) );
+
+	return pdm;
+
 
 }
 
 int main(){
 
+	fastio;
+
 	int n,q;
-
 	cin >> n;
-
+	
 	frr(i,n)
 		cin >> v[i];
 
 	build(1,1,n);
 
+	fr(i,82)
+		fr(j,82)
+			fr(k,82)
+				fr(l,163)
+					memo[i][j][k][l]=-5.0;
+
 	cin >> q;
 
 	frr(i,q){
 
-		int l,r,y,z;
+		int y,z,l,r;
 		cin >> l >> r >> y >> z;
 
 		vi aux = query(1,1,n,l,r);
 
-		vector<int>::iterator id = lower_bound(all(aux),y);
-		int amt = id - aux.begin();
+		vector<int>::iterator tot = lower_bound(all(aux),y);
+		
+		int boas = aux.end() - tot; //num de boas
+		int ruins = r-l+1 - boas;
 
-		//cout << amt << endl;
+		cout << setprecision(6) << fixed;
+		cout << solve(boas,ruins,z,80) << endl;	//80 eh offset
 
 	}
+
 
 }
