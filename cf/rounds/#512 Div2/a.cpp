4a8f3cb5ace49ba3dc0619ad38f2da06ea5ee56e
guidi <vitorguidi@gmail.com>
2018-09-23 13:50:35 -0300

    adicionado cf #512 div2

---
+++ cf/rounds/#512 Div2/a.cpp
@@ -0,0 +1,45 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+
+#define fr(i,n) 	for(int i=0;i<n;i++)
+#define frr(i,n)	for(int i=1;i<=n;i++)
+
+#define ms(x,i)	memset(x,i,sizeof(x))
+#define dbg(x)	cout << #x << " = " << x << endl
+#define all(x)	x.begin(),x.end()
+
+#define gnl cout << endl
+#define pira cout << "pira" << endl
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef pair<ll,ll> pll;
+
+const int INF = 0x3f3f3f3f;
+const ll llINF = 0x3f3f3f3f3f3f3f;
+
+int main(){
+    
+	fastio;
+
+	int n; cin >> n;
+
+	fr(i,n){
+		int x; cin >> x;
+		if(x){
+			cout << "HARD" << endl;
+			return 0;
+		}
+	}
+
+	cout << "EASY" << endl;
+
+}
+
