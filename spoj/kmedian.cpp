17d1b56199573972883b17243d2be30eb7044104
guidi <vitorguidi@gmail.com>
2019-01-12 21:33:24 -0200

    adicionado o resto da lista bit

---
+++ spoj/kmedian.cpp
@@ -0,0 +1,69 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+
+#define fr(i,n) 	for(int i=0;i<n;i++)
+#define frr(i,n)	for(int i=1;i<=n;i++)
+
+#define ms(x,i)	memset(x,i,sizeof(x))
+#define dbg(x)	cout << #x << " = " << x << endl
+#define all(x)	x.begin(),x.end()
+
+#define gnl cout << endl
+#define olar cout << "pira" << endl
+#define fastio 	ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef pair<ll,ll> pll; 
+
+const int INF = 0x3f3f3f3f;
+const ll llINF = 0x3f3f3f3f3f3f3f;
+
+ll bit[200200];
+ll v[100100];
+
+void update(int ind, int val){
+	for(int i=ind;i<200200;i+=i&-i)
+		bit[i]+=val;
+}
+
+ll query(int ind){
+	ll ans = 0;
+	for(int i=ind;i>0;i-=i&-i)
+		ans+=bit[i];
+	return ans;
+}
+
+int main(){
+
+	fastio;
+
+	int n,k;
+	cin >> n >> k;
+
+	frr(i,n){
+		cin >> v[i];
+		if(v[i]<k)	v[i]=-1;
+		else		v[i]=1;
+		v[i]+=v[i-1];
+	}
+
+	ll ans = 0;
+
+	update(100000,1);
+
+	for(int i=1;i<=n;i++){
+		ans+=query(v[i]+100000);
+		update(v[i]+100000,1);
+	}
+
+	cout << ans << endl;
+
+}
