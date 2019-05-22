58bb78e4f66a2ec8ff4e95886436a19d231e668f
Vitor <vitorguidi@gmail.com>
2019-05-22 15:31:19 -0300

    reatorei codigo velho pros bixos entenderem

--- Treinos equipe/xv de piracikobus/2018/11-30 IME Starters Tryouts 2018/f.cpp
+++ Treinos equipe/xv de piracikobus/2018/11-30 IME Starters Tryouts 2018/f.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 #define gnl cout << endl
 #define olar cout << "olar" << endl
-#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL) 
 
 typedef long long int ll;
 typedef pair<int,int> pii;
@@ -24,61 +24,54 @@ typedef vector<pii> vii;
 typedef pair<ll,ll> pll;
 
 const int INF = 0x3f3f3f3f;
-const ll llINF = 0x3f3f3f3f3f3f3f;
 const int MOD = 1e9+7;
 
-vi adj[112345];
-int memo[112345][2];
+vi adj[100100];
+int memo[100100][2];
 
-int pd (int v,int pint,int pai){
+int pd(int u, int flag, int pai){
 
-	int &pdm = memo[v][pint];
+    int& pdm = memo[u][flag];
+    if(pdm!=-1) return pdm;
 
-	if (pdm != -1) return (pdm);
+    int pega = 1;
+    int passa = 0;
 
-	int ans = 1;
-	for(int i = 0;i< adj[v].size();i++){
-		if (adj[v][i] == pai) continue;
-		ans += pd(adj[v][i],1,v);
-	}
+    for(auto v : adj[u]){
+        if(v==pai)  continue;
 
-	int ans2 = INF;
+        if(flag==0){
+            pega+=pd(v,1,u);
+            passa+=pd(v,0,u);
+        }
+        else{
+            passa+=pd(v,0,u);
+        }
+    }
 
-	if (pint == 1){
-		ans2 = 0;
-		for(int i = 0;i< adj[v].size();i++){
-			if(adj[v][i] == pai) continue;
-			ans2 += pd(adj[v][i],0,v);
-		}
-	}
-
-	ans = min (ans,ans2);
-	//dbg(v);
-	//dbg(ans);
-
-	pdm = ans;
-	return (pdm);
-
+    if(flag==0)
+        return pdm = max(pega, passa);
+    else
+        return pdm = passa;
 
 }
-
+    
 int main(){
 
 	fastio;
-	int n,k;
-	cin >> n >> k;
+    int n,k;
+    cin >> n >> k;
 
-	fr(i,k){
-		int a,b;
-		cin >> a >> b;
+    fr(i,k){
+        int a,b;
+        cin >> a >> b;
 
-		adj[a].pb(b);
-		adj[b].pb(a);
-	}
+        adj[a].pb(b);
+        adj[b].pb(a);
+    }
 
-	ms (memo,-1);
+    ms (memo,-1);
 
-	cout << n - pd(1,1,0)<<endl;
-
+    cout << pd(1,0,0)<<endl;
 
 }
